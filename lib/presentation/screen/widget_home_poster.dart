ce7e0c32749c400eb9359bba0c9e85976b7cb53e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-31 17:11:45 +0200

    Update widget_home_poster.dart

--- lib/presentation/screen/widget_home_poster.dart
+++ lib/presentation/screen/widget_home_poster.dart
@@ -2,4 +2,4 @@ import 'package:find_seat/model/entity/entity.dart';
 import 'package:find_seat/presentation/common_widgets/barrel_common_widgets.dart';
 import 'package:find_seat/presentation/custom_ui/custom_ui.dart';
 import 'package:find_seat/presentation/custom_ui/svg_image.dart';
-import 'package:find_
+import 'package:find
