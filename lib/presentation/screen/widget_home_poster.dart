14be5944b42b0edbad7538f4e45b8975db0a83c1
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-31 15:52:47 +0200

    Update widget_home_poster.dart

--- lib/presentation/screen/widget_home_poster.dart
+++ lib/presentation/screen/widget_home_poster.dart
@@ -28,4 +28,4 @@ class WidgetHomePosters extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: 
+      child:
