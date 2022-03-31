5eb69e94767728d72ecf9b6d61c9d0268eec7d37
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-31 19:32:37 +0200

    var item = items[index];

--- lib/presentation/screen/widget_home_poster.dart
+++ lib/presentation/screen/widget_home_poster.dart
@@ -61,3 +61,4 @@ class WidgetHomePosters extends StatelessWidget {
   _buildListPoster() {
     return WrapContentHozListView(
       itemBuilder: (context, index) {
+        var item = items[index];
