713628d3cb9d9f6495fc128b3119a8cb13b65480
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-31 16:37:59 +0200

    Update widget_home_poster.dart

--- lib/presentation/screen/widget_home_poster.dart
+++ lib/presentation/screen/widget_home_poster.dart
@@ -9,4 +9,4 @@ import 'package:flutter/material.dart';
 class WidgetHomePosters extends StatelessWidget {
   List<ItemPosterVM> items = [
     ItemPosterVM('images/events/912b4ea18698ad3b3519003c1c65c97e@2x.png',
-        'Happy Halloween 2K19', 'Mus
+        'Happy Halloween 2K19', 'Mu
