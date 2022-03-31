fe50bdde35ad084ead57b1b4b933cbbf5a234493
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-31 15:28:28 +0200

    Update widget_home_poster.dart

--- lib/presentation/screen/widget_home_poster.dart
+++ lib/presentation/screen/widget_home_poster.dart
@@ -105,4 +105,3 @@ class _WidgetItemPoster extends StatelessWidget {
             WidgetSpacer(height: 4),
             Text(item.title,
                 style: FONT_CONST.REGULAR_BLACK2_12,
-                maxLines: 2,
