461e15aaa664d25a6c275c0c499d6ea12e3a16dd
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-31 16:00:06 +0200

    Update widget_home_poster.dart

--- lib/presentation/screen/widget_home_poster.dart
+++ lib/presentation/screen/widget_home_poster.dart
@@ -22,4 +22,4 @@ class WidgetHomePosters extends StatelessWidget {
   String iconPath;
 
   WidgetHomePosters(
-      {required this.items, required this.label, require
+      {required this.items, required this.label, requir
