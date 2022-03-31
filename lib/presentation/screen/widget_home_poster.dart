4e71317484d46b65b092cbe2d06d34ec38626b65
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-31 19:35:05 +0200

    style: FONT_CONST.REGULAR_BLACK2_12,

--- lib/presentation/screen/widget_home_poster.dart
+++ lib/presentation/screen/widget_home_poster.dart
@@ -104,3 +104,4 @@ class _WidgetItemPoster extends StatelessWidget {
             ),
             WidgetSpacer(height: 4),
             Text(item.title,
+                style: FONT_CONST.REGULAR_BLACK2_12,
