978b007c8e236be38ee15f4edf36eb3244500864
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-31 18:48:45 +0200

    Update widget_home_poster.dart

--- lib/presentation/screen/widget_home_poster.dart
+++ lib/presentation/screen/widget_home_poster.dart
@@ -12,4 +12,4 @@ class WidgetHomePosters extends StatelessWidget {
         'Happy Halloween 2K19', 'Music show'),
     ItemPosterVM('images/events/ef0453e3c63b754f983e6863fa5c1583@2x.png',
         'Music DJ king monger Sertical Visa', 'Music show'),
-    ItemPosterVM('images/event
+    ItemPosterVM('images/events
