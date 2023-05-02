1f6325b7a06f23d7a6a332386f5e1255cd06f466
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-03 00:39:39 +0200

    Improrts

--- lib/presentation/router.dart
+++ lib/presentation/router.dart
@@ -2,3 +2,6 @@ import 'package:find_seat/model/entity/entity.dart';
 import 'package:find_seat/presentation/screen/all_shows/barrel_all_shows.dart';
 import 'package:find_seat/presentation/screen/booking/barrel_booking.dart';
 import 'package:find_seat/presentation/screen/booking/book_seat_slot/barrel_book_seat_slot.dart';
+import 'package:find_seat/presentation/screen/booking/book_seat_type/barrel_book_seat_type.dart';
+import 'package:find_seat/presentation/screen/list_all_cine/barrel_list_all_cine.dart';
+i
