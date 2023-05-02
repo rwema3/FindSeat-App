f517345b6a2d324ca50118c6ea71e612bc0f144f
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-03 00:39:54 +0200

    Remained Imports

--- lib/presentation/router.dart
+++ lib/presentation/router.dart
@@ -4,4 +4,11 @@ import 'package:find_seat/presentation/screen/booking/barrel_booking.dart';
 import 'package:find_seat/presentation/screen/booking/book_seat_slot/barrel_book_seat_slot.dart';
 import 'package:find_seat/presentation/screen/booking/book_seat_type/barrel_book_seat_type.dart';
 import 'package:find_seat/presentation/screen/list_all_cine/barrel_list_all_cine.dart';
+import 'package:find_seat/presentation/screen/list_my_ticket/sc_list_my_ticket.dart';
+import 'package:find_seat/presentation/screen/login/sc_login.dart';
+import 'package:find_seat/presentation/screen/register/barrel_register.dart';
+import 'package:find_seat/presentation/screen/show_info/barrel_show_info.dart';
+import 'package:flutter/material.dart';
+
+import 'screen/home/sc_home.dart';
 i
