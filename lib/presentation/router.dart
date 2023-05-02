3e8b823a271e29ed29526cb19fc929b6ac232beb
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-03 00:39:31 +0200

    Imports

--- lib/presentation/router.dart
+++ lib/presentation/router.dart
@@ -1,2 +1,4 @@
 import 'package:find_seat/model/entity/entity.dart';
 import 'package:find_seat/presentation/screen/all_shows/barrel_all_shows.dart';
+import 'package:find_seat/presentation/screen/booking/barrel_booking.dart';
+import 'package:find_seat/presentation/screen/booking/book_seat_slot/barrel_book_seat_slot.dart';
